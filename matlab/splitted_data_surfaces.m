91433926b7c89389596602555f14f39850649832
14Og <kolya.kiz@yandex.ru>
2023-09-26 16:32:44 +0300

    added matlab dir

---
+++ matlab/splitted_data_surfaces.m
@@ -0,0 +1,30 @@
+az_inc_0_180 = readmatrix("splitting_logs_output\az_inc_0_180.txt");
+az_inc_180_360 = readmatrix("splitting_logs_output\az_inc_180_360.txt");
+el_inc_min_0 = readmatrix("splitting_logs_output\el_inc_min_0.txt");
+el_inc_0_max = readmatrix("splitting_logs_output\el_inc_0_max.txt");
+
+
+
+x1 = az_inc_0_180(:,1);
+y1 = az_inc_0_180(:,2);
+z1 = az_inc_0_180(:,3);
+
+x2 = az_inc_180_360(:,1);
+y2 = az_inc_180_360(:,2);
+z2 = az_inc_180_360(:,3);
+
+x3 = el_inc_min_0(:,1);
+y3 = el_inc_min_0(:,2);
+z3 = el_inc_min_0(:,3);
+
+x4 = el_inc_0_max(:,1);
+y4 = el_inc_0_max(:,2);
+z4 = el_inc_0_max(:,3);
+
+% curveFitter(x1,y1,z1);
+% curveFitter(x2,y2,z2);
+% curveFitter(x3,y3,z3);
+% curveFitter(x4,y4,z4);
+curveFitter("matlab\figures\4_surfaces_curveFitter.sfit");
+
+
--- splitParser.py
+++ splitParser.py
@@ -1,6 +1,12 @@
 with  open("coordinateXLog.bin", "r+") as x_file, \
       open("coordinateYLog.bin", "r+") as y_file, \
-          open("splitting_logs_output/AzEllog.txt", "w+") as new_file:
+      open("splitting_logs_output/AzEllog.txt", "w+") as new_file, \
+      open("splitting_logs_output/az_inc_0_180.txt", "w") as file1, \
+      open("splitting_logs_output/az_inc_180_360.txt", "w") as file2, \
+      open("splitting_logs_output/el_inc_min_0.txt", "w") as file3, \
+      open("splitting_logs_output/el_inc_0_max.txt", "w") as file4:
+
+
         x_lines = [line[:-2:] for line in x_file.readlines()]
         x_lines[0] = x_lines[0][11::]
         x_lines = [s.split(" ") for s in x_lines]
@@ -37,12 +43,23 @@ with  open("coordinateXLog.bin", "r+") as x_file, \
         if __name__ == "__main__":
              
             for i in range(2):
-                new_file.write("az_inc_0_180 = [" if  i == 0 else "az_inc_180_360 = [" )
+                new_file.write("az_inc_0_180 = [" if  i == 0 else "az_inc_180_360 = [")
                 for vector in az_inc_0_180 if i == 0 else az_inc_180_360:
                     new_file.write(vector[0] + " ")
                     new_file.write(vector[1] + " ")
                     new_file.write(vector[2] + ";\n")
-
+                if i == 0:
+                     for vector in az_inc_0_180:
+                        for val in vector:
+                            file1.write(val + " ")
+                        file1.write("\n")
+                else:
+                    for vector in az_inc_180_360:
+                        for val in vector:
+                            file2.write(val + " ")
+                        file2.write("\n")
+                     
+                        
                 new_file.write("]\n\n")
             
             for i in range(2):
@@ -51,6 +68,20 @@ with  open("coordinateXLog.bin", "r+") as x_file, \
                     new_file.write(vector[0] + " ")
                     new_file.write(vector[1] + " ")
                     new_file.write(vector[2] + ";\n")
+                if i == 0:
+                    for vector in el_inc_min_0:
+                        for val in vector:
+                            file3.write(val + " ")
+                        file3.write("\n")
+                    else:
+                        for vector in el_inc_0_max:
+                            for val in vector:
+                                file4.write(val + " ")
+                            file4.write("\n")
+
+
+                    
+                    
 
                 new_file.write("]\n\n")
 
