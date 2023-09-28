1a964f56577982006c84725db4fd004fdce5fb51
14Og <kolya.kiz@yandex.ru>
2023-09-28 19:48:26 +0300

    added cartesian angles parsing opportunity

--- matlab/splitted_data_surfaces.m
+++ matlab/splitted_data_surfaces.m
@@ -8,14 +8,14 @@ el_inc_0_max = readmatrix("splitting_logs_output\el_inc_0_max.txt");
 x1 = az_inc_0_180(:,1);
 y1 = az_inc_0_180(:,2);
 z1 = az_inc_0_180(:,3);
-
-x2 = az_inc_180_360(:,1);
-y2 = az_inc_180_360(:,2);
-z2 = az_inc_180_360(:,3);
-
-x3 = el_inc_min_0(:,1);
-y3 = el_inc_min_0(:,2);
-z3 = el_inc_min_0(:,3);
+% 
+% x2 = az_inc_180_360(:,1);
+% y2 = az_inc_180_360(:,2);
+% z2 = az_inc_180_360(:,3);
+% 
+% x3 = el_inc_min_0(:,1);
+% y3 = el_inc_min_0(:,2);
+% z3 = el_inc_min_0(:,3);
 
 x4 = el_inc_0_max(:,1);
 y4 = el_inc_0_max(:,2);
@@ -25,8 +25,10 @@ curveFitter(x1,y1,z1);
 % curveFitter(x2,y2,z2);
 % curveFitter(x3,y3,z3);
 curveFitter(x4,y4,z4);
-% curveFitter("matlab\figures\4_surfaces_curveFitter.sfit");
-plot3(x1,y1,z1,'-o','Color','b','MarkerSize',10,...
-    'MarkerFaceColor','#D9FFFF');
+% plot3(x1,y1,z1,'o','Color','b','MarkerSize',10,...
+%     'MarkerFaceColor','#D9FFFF');
+% plot3(x4,y4,z4,'-o','Color','b','MarkerSize',10,...
+%     'MarkerFaceColor','#D9FFFF');
+
 
 
--- splitParser.py
+++ splitParser.py
@@ -1,3 +1,6 @@
+import math
+import sys
+
 with  open("coordinateXLog.bin", "r+") as x_file, \
       open("coordinateYLog.bin", "r+") as y_file, \
       open("splitting_logs_output/AzEllog.txt", "w+") as new_file, \
@@ -11,6 +14,10 @@ with  open("coordinateXLog.bin", "r+") as x_file, \
         x_lines[0] = x_lines[0][10::]
         x_lines = [s.split(" ") for s in x_lines]
         x_lines = x_lines[:-1]
+        if len(sys.argv) > 1 and sys.argv[1]  == "cartesian":
+            x_lines = [[i[0], str(round((180/math.pi)*math.acos(math.cos(float(i[1])*180/math.pi)*math.sin(float(i[2])*180/math.pi)),3)), \
+                    str(round((180/math.pi)*math.acos(math.sin(float(i[1])*180/math.pi)*math.sin(float(i[2])*180/math.pi)),3))] for i in x_lines]
+    
 
 
         y_lines = [line[:-2:] for line in y_file.readlines()]
@@ -18,24 +25,25 @@ with  open("coordinateXLog.bin", "r+") as x_file, \
         y_lines = [s.split(" ") for s in y_lines]
         y_lines = y_lines[:-1]
 
+
         az_inc = [[x_lines[i][0], y_lines[i][0], x_lines[i][2]] for i in range(len(x_lines))]
         el_inc = [[x_lines[i][0], y_lines[i][0], x_lines[i][1]] for i in range(len(x_lines))]
 
         az_inc.sort(key = lambda x: float(x[:][2]))
         el_inc.sort(key = lambda x: float(x[:][2]))
         for lst_az in range(len(az_inc)):
-              if int(az_inc[lst_az][2]) > 170:
+              if float(az_inc[lst_az][2]) > 180:
                     break
               
 
         for lst_el in range(len(el_inc)):
-              if int(el_inc[lst_el][2]) > 0:
+              if float(el_inc[lst_el][2]) > 0:
                     break
               
               
         
 
-        az_inc_0_180 = az_inc[:lst_az]
+        az_inc_0_180 = az_inc[::]
         az_inc_180_360 = az_inc[lst_az:]
         el_inc_min_0 = el_inc[:lst_el]
         el_inc_0_max = el_inc[::]
